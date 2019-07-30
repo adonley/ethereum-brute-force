7a93705d41d566d9835d1e04103b4165b0b86bb3
Andrew Donley <andrewjdonley@gmail.com>
2019-07-29 23:51:09 -0700

    even more simple

--- get-addresses.sh
+++ get-addresses.sh
@@ -3,8 +3,15 @@
 main () {
 	# Make addresses directory if it doesn't exist
 	if [ ! -d "addresses" ]; then
-		mkdir addresses
+		mkdir addresses;
 	fi
+
+	# Delete the addresses if it exists
+	if [ -f "addresses/addresses.csv" ]; then
+	  rm addresses/addresses.csv;
+	fi
+
+	touch addresses/addresses.csv;
 
 	# Download all the files that make up the addresses
 	for i in {0..9}; do 
@@ -19,9 +26,14 @@ main () {
 			echo "Decompressing part$i.csv";
 			gunzip -c addresses/part$i.gz > addresses/part$i.csv;
     fi
+
+    tail -n +2 "addresses/part$i.csv" >> addresses/addresses.csv;
+    rm "addresses/part$i.csv" "addresses/part$i.gz";
 	done
 
-	printf "Done grabbing address files.\n";
+  local LINES;
+  LINES=$(wc -l < addresses/addresses.csv)
+	printf "Done grabbing and parsing address files, %s addresses loaded.\n" "$LINES";
 }
 
 main;
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/crypto"
 	"io"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"os"
@@ -25,10 +24,10 @@ var path = "addresses"
 var partitions = 7
 var count int64
 var oldCount int64
-var addressesMap = concurrentMap { addresses: make(map[string]bool), }
+var addressesMap = concurrentMap{addresses: make(map[string]bool),}
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	count = int64(0)
 
@@ -40,7 +39,7 @@ func main() {
 		for {
 			select {
 			case <-checkTimer.C:
-				log.Printf("Checked: %d, Speed: %d per second", count, count - oldCount)
+				log.Printf("Checked: %d, Speed: %d per second", count, count-oldCount)
 				oldCount = count
 				checkTimer.Reset(value)
 			}
@@ -60,43 +59,19 @@ func main() {
 func loadAddresses() {
 	count := int64(0)
 
-	// TODO: Maybe we pull this in from the ENV for kubes?
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range files {
-
-		if strings.HasSuffix(f.Name(), "csv") {
-			log.Printf("Processing csv: %s\n", f.Name())
-			c, _ := os.Open(path + string(os.PathSeparator) + f.Name())
-			r := csv.NewReader(bufio.NewReader(c))
-			for {
-				record, err := r.Read()
-				if err == io.EOF {
-					break
-				}
-				count++
-
-				addressesMap.addresses[record[0]] = true
-			}
-			if err = c.Close(); err != nil {
-				log.Fatal(err)
-			}
+	c, _ := os.Open(path + string(os.PathSeparator) + "addresses.csv")
+	r := csv.NewReader(bufio.NewReader(c))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
 		}
+		count++
 
+		addressesMap.addresses[record[0]] = true
 	}
-
-	f, err := os.OpenFile(path + string(os.PathSeparator) + "balances.csv", os.O_CREATE | os.O_WRONLY, 0600)
-	defer f.Close()
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	for k := range addressesMap.addresses {
-		f.WriteString(k + "\n")
+	if err := c.Close(); err != nil {
+		log.Fatal(err)
 	}
 
 	log.Printf("Number of addresses loaded: %d", count)
@@ -114,7 +89,7 @@ func generateAddresses(seedPrivKey []byte) {
 	for ; ; {
 		// Move backward through those bytes
 		for i := 31; i > 0; i-- {
-			if seedPrivKey[i] + 1 == 255 {
+			if seedPrivKey[i]+1 == 255 {
 				seedPrivKey[i] = 0
 			} else {
 				seedPrivKey[i] += 1
